b146f3151b5ef6681e244f369745b530a0b3c0be
Notho-freedom <bobymomo6@gmail.com>
2025-04-11 13:57:59 +0200

    Split

--- Front/desktop/public/Electron/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("skyAPI", {
-  openWindow: (url, bounds) => ipcRenderer.invoke("open-view", url, bounds),
-});
